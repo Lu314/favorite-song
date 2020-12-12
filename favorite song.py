9fca5611a170548ad71d711b4315162d02f28447
Lucas Scalzo <l.scalzo90@gmail.com>
2020-12-12 12:56:38 -0600

    Add files via upload

---
+++ favorite song.py
@@ -0,0 +1,38 @@
+"""
+Song selected for favorite song assignment - dead presidents II by Jay Z 
+ 
+"""
+#Attributes: genre, name, artist, released, recorded, label, songwriter, producers, DurationInSeconds, sample, certification
+
+genre = "Rap"
+name = "Dead Presidents Two"
+artist = "Jay Z"
+released = "1996"
+recorded ="1995"
+label = "Roc-A-Fella"
+songwriter = "Shawn Carter"
+producers = "Ski"
+DurationInSeconds = "266 seconds"
+sample = "Lonnie Liston Smith, (A Garden of Peace)"
+certification = "Gold"
+
+print(genre)
+print(name)
+print(artist)
+print(released)
+print(recorded)
+print(label)
+print(songwriter)
+print(producers)
+print(DurationInSeconds)
+print(sample)
+print(certification)
+
+
+
+
+
+
+
+
+
